8514ff9c8aaf7ea2a059ca70504ed2d929eac2dc
sushmanth <sushmanthnatha@gmail.com>
2022-11-26 22:51:13 +0530

    rendering dynamic content

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ console.log(__dirname)
 console.log(path.join(__dirname,'../public'))
 
 const app = express()
+
+
+// Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname+'/../templates')
+
+// setup static dir to serve
 app.use(express.static(publicDirectoryPath))
 
 //this is no longer called when we call static page
@@ -23,6 +29,31 @@ app.use(express.static(publicDirectoryPath))
 // app.get('/about', (req, res)=>{
 //     res.send('<h1>About Weather</h1><br/><p>This app is about giving weather forecase for a given location by latlong or name of the place</p>')
 // })
+
+// setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'weather app',
+        name: 'sushmanth'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page',
+        name: 'Sush'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        question : 'How to request weather data for a location ?',
+        answer: 'just pass your location to our weather forecase endpoint'
+    })
+})
 
 app.get('/weather', (req,res)=>{
     res.send({
